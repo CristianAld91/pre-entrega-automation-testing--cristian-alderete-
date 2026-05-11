2465c4a87d0c14d8a864d29a202f65eab1bb3589
Cristian <cristianalderete911@gmail.com>
2026-05-11 20:46:59 -0300

    rama pom

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,11 +1,20 @@
 from page.login_page import LoginPage
 from selenium.webdriver.common.by import By
-
-def test_login(driver):
+from data.users import USERS
+import pytest
+@pytest.mark.parametrize("username, password", USERS [
+    ("standard_user", "secret_sauce"),
+    ("locked_out_user", "secret_sauce"),
+    ("problem_user", "secret_sauce"),
+    ("performance_glitch_user", "secret_sauce"),
+    ("error_user", "secret_sauce"),
+    ("visual_user", "secret_sauce")
+])
+def test_login(driver, username, password):
     try:
-        LoginPage(driver).login_page("standard_user", "secret_sauce")
+        LoginPage(driver).login_page(username, password)
         LoginPage(driver).open(
-            "standard_user", "secret_sauce"
+            username, password
         )
         
     except Exception as e:
